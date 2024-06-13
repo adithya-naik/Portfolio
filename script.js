e4f12683f8ce5898d692660ce39c3ba9eeea6ad3
adithya-naik <jatothadithyanaik@gmail.com>
2024-06-13 19:55:01 +0530

    started skills part

---
+++ script.js
@@ -0,0 +1,11 @@
+
+{/* <span id="element"></span> */}
+
+
+  var typed = new Typed('#tech-element', {
+    strings: ['C','JAVA','PYTHON','HTML','CSS','JAVA SCRIPT'],
+    typeSpeed: 50,
+    loop: true,
+    backSpeed: 50,
+    startDelay: 150,
+  });
