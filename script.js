9f1aebddba8a149040c5f9d2d0395f4563f3c9ff
adithya-naik <jatothadithyanaik@gmail.com>
2024-07-05 23:29:26 +0530

    updated css

--- script.js
+++ script.js
@@ -9,3 +9,10 @@
     backSpeed: 50,
     startDelay: 150,
   });
+  // var typed = new Typed('.name-js', {
+  //   strings: ['Web Developer'],
+  //   typeSpeed: 50,
+  //   loop: true,
+  //   backSpeed: 50,
+  //   // startDelay: 150,
+  // });
