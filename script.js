21cc2414b91ff3c6f0099f98941a263897ef296a
Jatoth Adithya Naik <jatothadithyanaik@gmail.com>
2024-08-16 19:25:09 +0530

    uploaded all files

---
+++ script.js
@@ -0,0 +1,18 @@
+
+{/* <span id="element"></span> */}
+
+
+  var typed = new Typed('#tech-element', {
+    strings: ['C','JAVA','PYTHON','HTML','CSS','JAVA SCRIPT','GitHub','BOOTSTRAP'],
+    typeSpeed: 40,
+    loop: true,
+    backSpeed: 20,
+    startDelay: 100,
+  });
+  // var typed = new Typed('.name-js', {
+  //   strings: ['Web Developer'],
+  //   typeSpeed: 50,
+  //   loop: true,
+  //   backSpeed: 50,
+  //   // startDelay: 150,
+  // });
